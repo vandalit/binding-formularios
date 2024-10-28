131d6398a104904579d548824cde6125f82d9f0d
Van <84937597+vandalit@users.noreply.github.com>
2024-10-27 22:28:17 -0300

    listo two way binding

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,139 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
-</script>
-
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+  <div id="app">
+    <form>
+      <div>
+        <label>Título de la tarjeta: </label>
+        <input type="text" v-model="tituloTarjeta" />
+      </div>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+
+      <div>
+        <label>Número: </label>
+        <input type="text" v-model="numeroTarjeta" placeholder="XXX XXX XXX XXX" />
+      </div>
+
+      <div>
+        <label>Fecha de expiración: </label>
+        <input type="text" v-model="fechaExpiracion" placeholder="MM/YY" />
+      </div>
+
+      <div>
+        <label>Propietario: </label>
+        <input type="text" v-model="propietario" />
+      </div>
+
+    </form>
+
+    <div class="carnet">
+      <h3>{{ tituloTarjeta }}</h3>
+      <img v-if="chipSrc" :src="chipSrc" width="40" alt="chip" class="chip" />
+      <div class="card-details">
+        <h2>{{ numeroTarjeta }}</h2>
+        <span>Fecha Exp: <b>{{ fechaExpiracion }}</b></span>
+      </div>
+      <footer>
+        <span>{{ propietario }}</span>
+        <img v-if="tipoTarjetaSrc" :src="tipoTarjetaSrc" width="60" alt="tipoTarjeta" />
+      </footer>
     </div>
-  </header>
-
-  <main>
-    <TheWelcome />
-  </main>
+  </div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
+<script>
+
+export default {
+  name: 'App',
+  data() {
+    return {
+      tituloTarjeta: "",
+      chipSrc: "/chip.png",
+      numeroTarjeta: "",
+      fechaExpiracion: "",
+      propietario: "",
+      tipoTarjetaSrc: "/public/visa.png",
+    };
+  },
+}
+</script>
+
+<style>
+* {
+  margin: 0;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+body {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 60vh;
+  font-size: 10px;
+  font-family: sans-serif;
 }
 
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
+#app {
+  display: flex;
+  gap: 40px;
+}
 
-  .logo {
-    margin: 0 2rem 0 0;
-  }
+form {
+  padding: 18px;
+  border-radius: 20px;
+  border: ridge 1px;
+  box-shadow: 1px 1px 1px 1px gray;
+  font-weight: bold;
+  display: flex;
+  flex-direction: column;
+  gap: 15px;
+  justify-content: center;
+}
 
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
+form div {
+  display: grid;
+  grid-template-columns: 100px 200px;
+  align-items: center;
+  gap: 5px;
+}
+
+form div label {
+  text-align: right;
+}
+
+.carnet {
+  width: 400px;
+  height: 230px;
+  background: #1c1a1e;
+  color: white;
+  border-radius: 20px;
+  padding: 20px;
+  box-shadow: 0px 0px 10px 2px black;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+}
+
+h3 {
+  font-size: 20px;
+}
+
+h2 {
+  font-size: 30px;
+  letter-spacing: 10px;
+  margin-bottom: 15px;
+  text-shadow: 0px 0px 1px gold;
+  font-family: Times;
+}
+
+b {
+  font-size: 16px;
+}
+
+footer {
+  display: flex;
+  justify-content: space-between;
+  align-items: flex-end;
+  font-weight: bold;
+  font-size: 18px;
+  text-transform: uppercase;
+  font-style: italic;
 }
 </style>
